90944860ea3ebe3e80edf56221aa2bd1996a69a9
Thomas Bartlett <tbartl@umich.edu>
2020-05-03 16:10:52 -0400

    Update readme

--- src/data/jobs.js
+++ src/data/jobs.js
@@ -1,7 +1,7 @@
 import { JOB as MINING_JOB } from "./mining";
-import { JOB as SCAVENGING_JOB } from "./scavenging";
+// import { JOB as SCAVENGING_JOB } from "./scavenging";
 
 export const ALL_JOBS = [
 	MINING_JOB,
-	SCAVENGING_JOB
+	// SCAVENGING_JOB
 ]
