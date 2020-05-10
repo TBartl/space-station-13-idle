bdc502e870525b37122e4a48df264215908235fc
Thomas Bartlett <tbartl@umich.edu>
2020-05-10 16:49:32 -0400

    Debug stuff

--- src/data/combat.js
+++ src/data/combat.js
@@ -8,6 +8,31 @@ export const ENEMIES = {
 		name: "Cargo Technician",
 		icon: require("@/assets/art/combat/enemies/cargo_tech.png"),
 		robustness: 6
+	},
+	debugA: {
+		name: "Debug A",
+		icon: require("@/assets/art/debug/A.png"),
+		robustness: 1
+	},
+	debugB: {
+		name: "Debug B",
+		icon: require("@/assets/art/debug/B.png"),
+		robustness: 2
+	},
+	debugC: {
+		name: "Debug C",
+		icon: require("@/assets/art/debug/C.png"),
+		robustness: 3
+	},
+	debugD: {
+		name: "Debug D",
+		icon: require("@/assets/art/debug/D.png"),
+		robustness: 4
+	},
+	debugE: {
+		name: "Debug E",
+		icon: require("@/assets/art/debug/E.png"),
+		robustness: 5
 	}
 }
 
@@ -16,6 +41,11 @@ export const ZONES = [
 		name: "Maintenance",
 		icon: require("@/assets/art/combat/zones/maint.png"),
 		enemies: ["mouse", "cargoTech"]
+	},
+	{
+		name: "Debug Land",
+		icon: require("@/assets/art/debug/banner.png"),
+		enemies: ["debugA", "debugB", "debugC", "debugD", "debugE"]
 	}
 ]
 
