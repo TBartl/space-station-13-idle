7208a688a7a98135d71ef6ff76102413d91414ac
Nabski89 <Nabski@hotmail.com>
2020-05-11 23:44:55 -0500

    TestGuns

--- src/data/engineering.js
+++ src/data/engineering.js
@@ -4,6 +4,41 @@ export const ITEMS = new Map([
 		sellPrice: 0,
 		icon: require("@/assets/art/engineering/power.png")
 	}],
+	["meleesmall", {
+		name: "Claw",
+		sellPrice: 0,
+		icon: require("@/assets/art/engineering/meleesmall.png")
+	}],
+	["meleebig", {
+		name: "Drill",
+		sellPrice: 0,
+		icon: require("@/assets/art/engineering/meleebig.png")
+	}],
+	["gunsmall", {
+		name: "Mounted Rifle",
+		sellPrice: 0,
+		icon: require("@/assets/art/engineering/gunsmall.png")
+	}],
+	["ammosmall", {
+		name: "Rifle Ammo",
+		sellPrice: 0,
+		icon: require("@/assets/art/engineering/ammosmall.png")
+	}],
+	["gunbig", {
+		name: "Mounted Cannon",
+		sellPrice: 0,
+		icon: require("@/assets/art/engineering/gunbig.png")
+	}],
+	["ammobig", {
+		name: "Cannon Ammo",
+		sellPrice: 0,
+		icon: require("@/assets/art/engineering/ammobig.png")
+	}],
+	["gunenergy", {
+		name: "Energy Rifle",
+		sellPrice: 0,
+		icon: require("@/assets/art/engineering/gunenergy.png")
+	}],
 ]);	
 
 export const ACTIONS = {
@@ -14,6 +49,55 @@ export const ACTIONS = {
 		icon: require("@/assets/art/engineering/bicycle.png"),
 		xp: 5,
 		requiredLevel: 1
+	},
+	engMeleesmall: {
+		time: 1,
+		item: "meleesmall",
+		icon: require("@/assets/art/engineering/meleesmall.png"),
+		xp: 5,
+		requiredLevel: 2
+	},
+	engMeleebig: {
+		time: 1,
+		item: "meleebig",
+		icon: require("@/assets/art/engineering/meleebig.png"),
+		xp: 5,
+		requiredLevel: 3
+	},
+	engGunsmall: {
+		time: 1,
+		item: "gunsmall",
+		icon: require("@/assets/art/engineering/gunsmall.png"),
+		xp: 5,
+		requiredLevel: 4
+	},
+	engAmmosmall: {
+		time: 1,
+		item: "ammosmall",
+		icon: require("@/assets/art/engineering/ammosmall.png"),
+		xp: 5,
+		requiredLevel: 5
+	},
+	engGunbig: {
+		time: 1,
+		item: "gunbig",
+		icon: require("@/assets/art/engineering/gunbig.png"),
+		xp: 5,
+		requiredLevel: 6
+	},
+	engAmmobig: {
+		time: 1,
+		item: "ammobig",
+		icon: require("@/assets/art/engineering/ammobig.png"),
+		xp: 5,
+		requiredLevel: 7
+	},
+	engGunenergy: {
+		time: 1,
+		item: "gunenergy",
+		icon: require("@/assets/art/engineering/gunenergy.png"),
+		xp: 5,
+		requiredLevel: 8
 	},
 }
 
