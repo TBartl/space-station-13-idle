adbe7664d4c3dac1b772f451408cdf7a47bb6c79
Nabski89 <Nabski@hotmail.com>
2020-05-22 00:12:19 -0400

    ALL THE NEW JOBS

---
+++ src/components/Content/ContentSecurity.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <content-header :text="job.name" :icon="job.icon" :color="job.color" />
+    <div class="content-container">
+      <div class="row">
+        <div class="col-12 mb-4">
+          <experience-header :color="job.color" :jobId="jobId" />
+        </div>
+        <div class="col-6 col-md-4 col-lg-3 col-xl-2" v-for="[actionId, action] in viewableActions" :key="actionId">
+          <generic-action
+            :jobId="jobId"
+            :actionName="'ARREST'"
+            :action="action"
+            :actionId="actionId"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { findLastIndex } from "lodash";
+import { JOB, ACTIONS } from "@/data/security";
+import ContentAbstract from "@/components/Content/ContentAbstract";
+import ExperienceHeader from "@/components/Content/ExperienceHeader";
+import GenericAction from "@/components/Content/GenericAction";
+import { mapState } from "vuex";
+export default {
+  extends: ContentAbstract,
+  components: { GenericAction, ExperienceHeader },
+  computed: {
+    jobId() {
+      return "security";
+    },
+    ...mapState({
+      level(state, getters) {
+        return getters[this.jobId + "/level"];
+      }
+    }),
+    job() {
+      return JOB;
+    },
+    viewableActions() {
+      let entries = Object.entries(ACTIONS);
+      let lastActionable = findLastIndex(entries, entry => {
+        return this.level >= entry[1].requiredLevel;
+      });
+
+      return entries.slice(0, lastActionable + 2);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/state/security.js
@@ -0,0 +1,15 @@
+import { cloneDeep, merge } from 'lodash';
+import jobBase from '@/state/jobBase';
+import jobSingleAction from '@/state/jobSingleAction';
+
+import { ACTIONS } from "@/data/security"
+
+const security = merge(cloneDeep(jobBase), cloneDeep(jobSingleAction), {
+	getters: {
+		jobActions() {
+			return ACTIONS;
+		}
+	}
+});
+
+export default security;
