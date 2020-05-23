eebbbd292f303e7784aa4ba6983c70118e6b5ee3
Nabski89 <Nabski@hotmail.com>
2020-05-22 23:26:59 -0400

    Face Items

--- src/data/items/slotFace.js
+++ src/data/items/slotFace.js
@@ -15,5 +15,193 @@ export default {
 		requires: {
 			evasion: 3,
 		}
-	}
+	},
+	faceMedhud: {
+		name: "Health HUD Glasses",
+		sellPrice: 1200,
+		icon: require("@/assets/art/combat/items/face_healthhud.png"),
+		equipmentSlot: "face",
+		overlay: require("@/assets/art/combat/items/face_healthhud_overlay.png"),
+		stats: {
+			maxHealth: 50,
+			evasion: 30,
+			protection: 20,
+			precision: 10,
+			power: 10,
+		},
+		requires: {
+			evasion: 3,
+		}
+	},
+	faceSecglasses: {
+		name: "Meson Goggles",
+		sellPrice: 1200,
+		icon: require("@/assets/art/combat/items/face_secglass.png"),
+		equipmentSlot: "face",
+		overlay: require("@/assets/art/combat/items/face_secglass_overlay.png"),
+		stats: {
+			maxHealth: 50,
+			evasion: 30,
+			protection: 20,
+			precision: 10,
+			power: 10,
+		},
+		requires: {
+			evasion: 3,
+		}
+	},
+	faceScience: {
+		name: "Science Goggles",
+		sellPrice: 1200,
+		icon: require("@/assets/art/combat/items/face_science.png"),
+		equipmentSlot: "face",
+		overlay: require("@/assets/art/combat/items/face_science_overlay.png"),
+		stats: {
+			maxHealth: 50,
+			evasion: 30,
+			protection: 20,
+			precision: 10,
+			power: 10,
+		},
+		requires: {
+			evasion: 3,
+		}
+	},
+	faceTiki: {
+		name: "Tiki Mask",
+		sellPrice: 1200,
+		icon: require("@/assets/art/combat/items/face_tiki.png"),
+		equipmentSlot: "face",
+		overlay: require("@/assets/art/combat/items/face_tiki_overlay.png"),
+		stats: {
+			maxHealth: 50,
+			evasion: 30,
+			protection: 20,
+			precision: 10,
+			power: 10,
+		},
+		requires: {
+			evasion: 3,
+		}
+	},
+	faceWestle: {
+		name: "Wrestling Mask",
+		sellPrice: 1200,
+		icon: require("@/assets/art/combat/items/face_wrestle.png"),
+		equipmentSlot: "face",
+		overlay: require("@/assets/art/combat/items/face_wrestle_overlay.png"),
+		stats: {
+			maxHealth: 50,
+			evasion: 30,
+			protection: 20,
+			precision: 10,
+			power: 10,
+		},
+		requires: {
+			evasion: 3,
+		}
+	},
+	faceGas: {
+		name: "Gas Mask",
+		sellPrice: 1200,
+		icon: require("@/assets/art/combat/items/face_gas.png"),
+		equipmentSlot: "face",
+		overlay: require("@/assets/art/combat/items/face_gas_overlay.png"),
+		stats: {
+			maxHealth: 50,
+			evasion: 30,
+			protection: 20,
+			precision: 10,
+			power: 10,
+		},
+		requires: {
+			evasion: 3,
+		}
+	},
+	faceFox: {
+		name: "Fox Mask",
+		sellPrice: 1200,
+		icon: require("@/assets/art/combat/items/face_fox.png"),
+		equipmentSlot: "face",
+		overlay: require("@/assets/art/combat/items/face_fox_overlay.png"),
+		stats: {
+			maxHealth: 50,
+			evasion: 30,
+			protection: 20,
+			precision: 10,
+			power: 10,
+		},
+		requires: {
+			evasion: 3,
+		}
+	},
+	faceSec: {
+		name: "Security Hailer",
+		sellPrice: 1200,
+		icon: require("@/assets/art/combat/items/face_sec.png"),
+		equipmentSlot: "face",
+		overlay: require("@/assets/art/combat/items/face_sec_overlay.png"),
+		stats: {
+			maxHealth: 50,
+			evasion: 30,
+			protection: 20,
+			precision: 10,
+			power: 10,
+		},
+		requires: {
+			evasion: 3,
+		}
+	},
+	faceMeson: {
+		name: "Syndicate Mask",
+		sellPrice: 1200,
+		icon: require("@/assets/art/combat/items/face_syndicat.png"),
+		equipmentSlot: "face",
+		overlay: require("@/assets/art/combat/items/face_syndicate_overlay_anim.gif"),
+		stats: {
+			maxHealth: 50,
+			evasion: 30,
+			protection: 20,
+			precision: 10,
+			power: 10,
+		},
+		requires: {
+			evasion: 3,
+		}
+	},
+	faceMime: {
+		name: "Mime Mask",
+		sellPrice: 1200,
+		icon: require("@/assets/art/combat/items/face_mime.png"),
+		equipmentSlot: "face",
+		overlay: require("@/assets/art/combat/items/face_mime_overlay.png"),
+		stats: {
+			maxHealth: 50,
+			evasion: 30,
+			protection: 20,
+			precision: 10,
+			power: 10,
+		},
+		requires: {
+			evasion: 3,
+		}
+	},
+	faceClown: {
+		name: "Clown Mask",
+		sellPrice: 1200,
+		icon: require("@/assets/art/combat/items/face_clown.png"),
+		equipmentSlot: "face",
+		overlay: require("@/assets/art/combat/items/face_mime_overlay.png"),
+		stats: {
+			maxHealth: 50,
+			evasion: 30,
+			protection: 20,
+			precision: 10,
+			power: 10,
+		},
+		requires: {
+			evasion: 3,
+		}
+	},
+
 }
