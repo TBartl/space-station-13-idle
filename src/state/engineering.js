976d11021fb84c470a570fdcc1a187a2c7f76845
Thomas Bartlett <tbartl@umich.edu>
2020-05-24 13:04:52 -0400

    Engineering upgrades

--- src/state/engineering.js
+++ src/state/engineering.js
@@ -5,12 +5,16 @@ import jobSingleAction from '@/state/jobSingleAction';
 import { ACTIONS } from "@/data/engineering"
 
 const engineering = merge(cloneDeep(jobBase), cloneDeep(jobSingleAction), {
-	getters:{
+	getters: {
 		jobId() {
 			return "engineering";
 		},
 		baseActions(state, getters, rootState, rootGetters) {
 			let actions = cloneDeep(ACTIONS);
+			let upgradeCount = rootGetters["upgrades/get"]("cableManagement");
+			for (let action of Object.values(actions)) {
+				action.xp = Math.round(action.xp * (1 + upgradeCount * .15));
+			}
 			return actions;
 		}
 	}
