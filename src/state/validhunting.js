1e1b1a94122a21c0d2234f8c8b522c6be3fc0723
Thomas Bartlett <tbartl@umich.edu>
2020-05-31 17:45:33 -0400

    Finish validhunting (WIP numbers)

--- src/state/validhunting.js
+++ src/state/validhunting.js
@@ -1,11 +1,13 @@
 import { cloneDeep, merge } from 'lodash';
 import jobBase from '@/state/jobBase';
+import ENEMIES from "@/data/enemies";
+import { calcRobustness } from "@/utils/combatUtils";
 
 const validhunting = merge(cloneDeep(jobBase), {
 	state: {
 		enemyId: 'mouse',
-		count: 3,
-		xpReward: 500
+		count: 10,
+		xpReward: 250
 	},
 	getters: {
 		targetEnemyId(state) {
@@ -22,8 +24,14 @@ const validhunting = merge(cloneDeep(jobBase), {
 		lowerCount(state) {
 			state.count = Math.max(state.count - 1, 0);
 		},
-		getNewTask(state){
-			state.count = 5;
+		setNewCount(state, count) {
+			state.count = count;
+		},
+		setNewEnemy(state, enemyId) {
+			state.enemyId = enemyId;
+		},
+		setNewXpReward(state, xpReward) {
+			state.xpReward = xpReward;
 		}
 	},
 	actions: {
@@ -32,10 +40,25 @@ const validhunting = merge(cloneDeep(jobBase), {
 				commit("lowerCount");
 			}
 		},
-		completeTask({ state, commit }) {
+		completeTask({ state, commit, getters }) {
 			if (state.count > 0) return;
 			commit("addXP", state.xpReward);
-			commit("getNewTask");
+			// Get a new task
+			let minCount = 10;
+			let maxAddedCount = minCount + getters.level * 6;
+			let count = minCount + Math.round(Math.random() * maxAddedCount);
+			commit("setNewCount", count);
+
+			let highestLevel = 3 + getters.level * 2;
+			let filteredEnemies = Object.entries(ENEMIES).filter(pair => {
+				return calcRobustness(pair[1].stats, "enemy") <= highestLevel;
+			});
+			var pickedEnemy = filteredEnemies[Math.floor(Math.random() * filteredEnemies.length)];
+			commit("setNewEnemy", pickedEnemy[0]);
+
+			let pickedEnemyRobustness = calcRobustness(pickedEnemy[1].stats, "enemy");
+			let xpReward = Math.round(Math.max(pickedEnemyRobustness, 3) * count * 20);
+			commit("setNewXpReward", xpReward);
 
 		}
 	}
