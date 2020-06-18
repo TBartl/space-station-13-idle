96b0656ade76765a9d5f4b3e24d1b7b1c99d4b80
Thomas Bartlett <tbartl@umich.edu>
2020-06-18 01:24:42 -0400

    sort tinkering, add potion slot

--- src/components/Content/ContentTinkering.vue
+++ src/components/Content/ContentTinkering.vue
@@ -2,11 +2,27 @@
   <div>
     <content-header :text="job.name" :icon="job.icon" :color="job.color" />
     <div class="content-container">
-      <div class="row">
-        <div class="col-12 mb-4">
+      <div class="row mb-4">
+        <div class="col-md-8 col-lg-9 col-xl-10">
           <experience-header :color="job.color" :jobId="jobId" />
         </div>
-        <div class="col-6 col-md-4 col-lg-3 col-xl-2" v-for="[actionId, action] in viewableActions" :key="actionId">
+        <div class="col-md-4 col-lg-3 col-xl-2">
+          <potion-header :jobId="jobId" />
+        </div>
+      </div>
+      <div
+        class="tier row"
+        v-for="(typedEntry, tier) in Object.entries(viewableTypedActionEntries)"
+        :key="tier"
+      >
+        <div class="col-12">
+          <span class="type-text text-uppercase">{{typedEntry[0]}}</span>
+        </div>
+        <div
+          class="col-6 col-md-4 col-lg-3 col-xl-2"
+          v-for="[actionId, action] in typedEntry[1]"
+          :key="actionId"
+        >
           <generic-action
             :jobId="jobId"
             :actionName="'CRAFT'"
@@ -41,12 +57,26 @@ export default {
     job() {
       return JOB;
     },
-    viewableActions() {
-			return this.$store.getters[this.jobId + "/filteredActionEntries"];
+    viewableTypedActionEntries() {
+      let entries = this.$store.getters[this.jobId + "/filteredActionEntries"];
+
+      let toReturn = {}; // type: [entries]
+      for (let entry of entries) {
+        let type = entry[1].type;
+        if (!toReturn[type]) toReturn[type] = [entry];
+        else toReturn[type].push(entry);
+      }
+
+      return toReturn;
     }
   }
 };
 </script>
 
 <style scoped>
+.type-text {
+  font-size: 20;
+  font-weight: bold;
+  color: rgba(245, 245, 245, 0.555);
+}
 </style>
