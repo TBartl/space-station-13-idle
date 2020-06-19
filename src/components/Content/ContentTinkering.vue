48800e8fa7fd5254f5047bf6f7ae2b787ae738d3
Thomas Bartlett <tbartl@umich.edu>
2020-06-19 16:11:53 -0400

    Fix potion header for tinkering

--- src/components/Content/ContentTinkering.vue
+++ src/components/Content/ContentTinkering.vue
@@ -40,11 +40,12 @@ import { findLastIndex } from "lodash";
 import { JOB } from "@/data/tinkering";
 import ContentAbstract from "@/components/Content/ContentAbstract";
 import ExperienceHeader from "@/components/Content/ExperienceHeader";
+import PotionHeader from "@/components/Content/PotionHeader";
 import GenericAction from "@/components/Content/GenericAction";
 import { mapState } from "vuex";
 export default {
   extends: ContentAbstract,
-  components: { GenericAction, ExperienceHeader },
+  components: { GenericAction, ExperienceHeader, PotionHeader },
   computed: {
     jobId() {
       return "tinkering";
