3cb3073a8d7b2ccc3e10109dbce40ae4c7c70936
Thomas Bartlett <tbartl@umich.edu>
2020-06-19 21:12:34 -0400

    Functional tinkering potion

--- src/state/tinkering.js
+++ src/state/tinkering.js
@@ -15,15 +15,36 @@ const tinkering = merge(cloneDeep(jobBase), cloneDeep(jobSingleAction), {
 		baseActions(state, getters, rootState, rootGetters) {
 			let actions = cloneDeep(ACTIONS);
 
-			
-			let potion = rootGetters["potions/get"]("mining");
+			let potion = rootGetters["potions/get"]("tinkering");
 			let potionItemId = potion ? potion.itemId : null;
 
-			
-			if (potionItemId == "potionTinkering") {
-				// TODO:
-			}
 
+			if (potionItemId == "potionTinkering") {
+				for (let action of Object.values(actions)) {
+					// TODO:
+					let originalItem = action.item;
+					delete action.item;
+					action.itemTables = [
+						{
+							chance: 1,
+							item: originalItem
+						},
+						{
+							chance: .25,
+							itemTable: []
+						}
+					]
+					for (let junkId of ALL_JUNKS) {
+						if (!action.requiredItems) continue;
+						if (junkId == Object.keys(action.requiredItems)[0]) continue;
+						action.itemTables[1].itemTable.push({
+							id: junkId,
+							weight: 1
+						});
+					}
+				}
+
+			}
 			return actions;
 		}
 	}
