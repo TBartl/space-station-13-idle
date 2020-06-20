e0f06b970c231c71520a898cfca0d0b3f6d638cb
Thomas Bartlett <tbartl@umich.edu>
2020-06-20 15:15:38 -0400

    functional cooking items

--- src/state/cooking.js
+++ src/state/cooking.js
@@ -3,6 +3,8 @@ import jobBase from '@/state/jobBase';
 import jobSingleAction from '@/state/jobSingleAction';
 
 import { ACTIONS } from "@/data/cooking"
+import { COOKING_UPGRADE_PERCENT } from "@/data/upgrades";
+import { COOKING_POTION_PERCENT } from '@/data/items/resourceChemistry';
 
 const cooking = merge(cloneDeep(jobBase), cloneDeep(jobSingleAction), {
 	getters: {
@@ -11,6 +13,36 @@ const cooking = merge(cloneDeep(jobBase), cloneDeep(jobSingleAction), {
 		},
 		baseActions(state, getters, rootState, rootGetters) {
 			let actions = cloneDeep(ACTIONS);
+
+			let upgradeCount = rootGetters["upgrades/get"]("fryCooking");
+			let potion = rootGetters["potions/get"]("cooking");
+			let potionItemId = potion ? potion.itemId : null;
+
+			if (upgradeCount || potionItemId) {
+
+				let actionEntries = Object.values(actions);
+				actionEntries.forEach((action) => {
+
+					let originalItem = action.item;
+					delete action.item;
+
+					action.itemTables = [
+						{
+							chance: 1 - upgradeCount * COOKING_UPGRADE_PERCENT,
+							item: originalItem
+						}
+					]
+					delete action.item;
+
+					if (upgradeCount) {
+						action.itemTables.push({
+							chance: upgradeCount * COOKING_UPGRADE_PERCENT,
+							item: "q_" + originalItem
+						})
+					}
+				})
+			}
+
 			return actions;
 		},
 		locked(state, getters, rootState, rootGetters) {
