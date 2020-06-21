7926fbf8964d9d1bea591e50520c60fff53d88e1
Thomas Bartlett <tbartl@umich.edu>
2020-06-21 16:38:57 -0400

    Update shitposting with potions

--- src/components/Content/ContentShitposting.vue
+++ src/components/Content/ContentShitposting.vue
@@ -2,10 +2,38 @@
   <div>
     <content-header :text="job.name" :icon="job.icon" :color="job.color" />
     <div class="content-container">
-      <div class="row">
-        <div class="col-12 mb-4">
+      <div class="row mb-2">
+        <div class="col-md-8 col-lg-9 col-xl-10">
           <experience-header :color="job.color" :jobId="jobId" />
         </div>
+        <div class="col-md-4 col-lg-3 col-xl-2">
+          <potion-header :jobId="jobId" />
+        </div>
+      </div>
+
+      <job-info
+        infoId="REPLACEME"
+        :icon="require('@/assets/art/jobinfo/REPLACEME.png')"
+        title="REPLACEME the REPLACEME says..."
+        :options="[
+					{name: 'Back'},
+					{name: 'REPLACEME', icon: require('@/assets/art/debug/A.png'), iconClass:'mx--1'}
+				]"
+      >
+        <template slot="Back">
+          <span>REPLACEME</span>
+          <span>
+            Example
+            <img class="mx--2" :src="require('@/assets/art/mining/SheetIron.png')" />
+            <b>Weighted</b>.
+          </span>
+        </template>
+        <template slot="REPLACEME">
+          <span>REPLACEME</span>
+        </template>
+      </job-info>
+
+      <div class="row">
         <div
           class="col-6 col-md-4 col-lg-3 col-xl-2"
           v-for="[actionId, action] in viewableActions"
@@ -28,11 +56,12 @@ import { findLastIndex } from "lodash";
 import { JOB } from "@/data/shitposting";
 import ContentAbstract from "@/components/Content/ContentAbstract";
 import ExperienceHeader from "@/components/Content/ExperienceHeader";
+import PotionHeader from "@/components/Content/PotionHeader";
 import GenericAction from "@/components/Content/GenericAction";
 import { mapState } from "vuex";
 export default {
   extends: ContentAbstract,
-  components: { GenericAction, ExperienceHeader },
+  components: { GenericAction, ExperienceHeader, PotionHeader },
   computed: {
     jobId() {
       return "shitposting";
