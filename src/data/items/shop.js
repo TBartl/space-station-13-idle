ce40e38161a8f5e72809b4d74f0c68a5d97347a0
Nabski89 <Nabski@hotmail.com>
2020-06-20 23:32:30 -0400

    Slime Stats and Crates

--- src/data/items/shop.js
+++ src/data/items/shop.js
@@ -3,6 +3,11 @@ export default {
 		name: "Space Coins",
 		icon: require("@/assets/art/misc/coin-padded.png")
 	},
+	telecrystal: {
+		name: "Telecrystal",
+		sellPrice: 500,
+		icon: require("@/assets/art/shop/items/telecrystal.png")
+	},
 	hatCrate: {
 		name: "Hat Crate",
 		icon: require("@/assets/art/shop/items/hatcrate.png"),
@@ -33,6 +38,62 @@ export default {
 			},
 			{
 				id: 'headRadio4',
+				weight: 1
+			}
+		]
+	},
+	knifeCrate: {
+		name: "Knife Crate",
+		icon: require("@/assets/art/shop/items/weaponcrate.png"),
+		itemTable: [
+			{
+				id: 'headCake',
+				weight: 1
+			},
+			{
+				id: 'headH1',
+				weight: 1
+			}
+		]
+	},
+	lavaCrate: {
+		name: "Local Heritage Crate",
+		icon: require("@/assets/art/shop/items/necrocrate.png"),
+		itemTable: [
+			{
+				id: 'headCake',
+				weight: 1
+			},
+			{
+				id: 'headH1',
+				weight: 1
+			}
+		]
+	},
+	supplyCrate: {
+		name: "Supply Droppod",
+		icon: require("@/assets/art/shop/items/droppod.png"),
+		itemTable: [
+			{
+				id: 'headCake',
+				weight: 1
+			},
+			{
+				id: 'headH1',
+				weight: 1
+			}
+		]
+	},
+	syndieCrate: {
+		name: "Hijacked Droppod",
+		icon: require("@/assets/art/shop/items/syndiedroppod.png"),
+		itemTable: [
+			{
+				id: 'headCake',
+				weight: 1
+			},
+			{
+				id: 'headH1',
 				weight: 1
 			}
 		]
