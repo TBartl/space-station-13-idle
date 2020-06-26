b76d74f0d9e7e0aad83259d3dac9782bdd266126
Thomas Bartlett <tbartl@umich.edu>
2020-06-26 19:43:45 -0400

    Chemistry dialogue

--- src/components/Content/ContentChemistry.vue
+++ src/components/Content/ContentChemistry.vue
@@ -17,19 +17,102 @@
         title="Mikal Blak the Chemist says..."
         :options="[
 					{name: 'Back'},
-					{name: 'REPLACEME', icon: require('@/assets/art/debug/A.png'), iconClass:'mx--1'}
+					{name: 'Power?', icon: require('@/assets/art/engineering/power.png'), iconClass:'mx--0'},
+					{name: 'Bases?', icon: require('@/assets/art/chemistry/chemOil.png'), iconClass:'mx--1'},
+					{name: 'CHEMs?', icon: require('@/assets/art/chemistry/faunaPerfume.png'), iconClass:'mx--1'},
+					{name: 'Combat?', icon: require('@/assets/art/combat/precision.png'), iconClass:'mx--0'},
 				]"
       >
         <template slot="Back">
-          <span>REPLACEME</span>
+          <span>Flourine, Uranium, Carbon, Potassium, Arsenic, Sulfur...</span>
+          <span>I'm sorry, did you need something?</span>
+        </template>
+        <template slot="Power?">
           <span>
-            Example
-            <img class="mx--2" :src="require('@/assets/art/mining/SheetIron.png')" />
-            <b>Weighted</b>.
+            These chem dispensers use up a lot of
+            <img
+              class="mx--0"
+              :src="require('@/assets/art/engineering/power.png')"
+            />
+            <b>Power</b>.
+          </span>
+          <span>
+            You'll need to
+            <i>periodically</i> check up on the
+            <img class="mx--2" :src="require('@/assets/art/engineering/icon.png')" />
+            <b>Engineers</b> to make sure you're stocked up.
           </span>
         </template>
-        <template slot="REPLACEME">
-          <span>REPLACEME</span>
+        <template slot="Bases?">
+          <span>
+            Before you can make any
+            <img
+              class="mx--2"
+              :src="require('@/assets/art/chemistry/faunaPerfume.png')"
+            />
+            <b>CHEMms</b>, you'll need to synthesize the individual
+            <img
+              class="mx--2"
+              :src="require('@/assets/art/chemistry/chemOil.png')"
+            />
+            <b>Bases</b> that make it up.
+          </span>
+          <span>
+            You can speed up this process with
+            <img
+              class="mx--0"
+              :src="require('@/assets/art/chemistry/upgrade1.png')"
+            />
+            <b>Upgrades</b> from
+            <img class="mx--0" :src="require('@/assets/art/sidebar/cargo.png')" />
+            <b>Cargo</b>.
+          </span>
+        </template>
+        <template slot="CHEMs?">
+          <span>
+            <img class="mx--2" :src="require('@/assets/art/chemistry/faunaPerfume.png')" />
+            <b>CHEMs</b> provide unique bonuses to individual
+            <img
+              class="mx--0"
+              :src="require('@/assets/art/mining/icon.png')"
+            />
+            <img class="mx--0" :src="require('@/assets/art/engineering/icon.png')" />
+            <img class="mx--0" :src="require('@/assets/art/botany/icon.png')" />
+            <b>Jobs</b>.
+          </span>
+          <span>
+            Each
+            <img class="mx--2" :src="require('@/assets/art/chemistry/faunaPerfume.png')" />
+            <b>CHEM</b> you make will have a limited number of
+            <img
+              class="mx--2"
+              :src="require('@/assets/art/chemistry/faunaPerfume.png')"
+            />
+            <b>CHEM</b> before it depletes.
+          </span>
+          <span>
+            You can equip
+            <img
+              class="mx--2"
+              :src="require('@/assets/art/chemistry/faunaPerfume.png')"
+            />
+            <b>CHEMs</b> near the top section of each job.
+          </span>
+        </template>
+        <template slot="Combat?">
+          <span>
+            All these specialized
+            <img class="mx--2" :src="require('@/assets/art/chemistry/faunaPerfume.png')" />
+            <b>CHEMs</b> and you
+            <i>also</i> want something for
+            <img
+              class="mx--0"
+              :src="require('@/assets/art/combat/precision.png')"
+            />
+            <b>Combat?</b>
+          </span>
+          <span>Man, the technology just isn't there for that.</span>
+          <span>I mean uhh... that sounds dangerous and I don't want to get involved with anything like that.</span>
         </template>
       </job-info>
 
