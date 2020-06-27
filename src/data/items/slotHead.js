c2b86fd229b2306585dca2f8722bb832e414f5b4
Nabski89 <Nabski@hotmail.com>
2020-06-26 23:12:33 -0400

    Hat Crate

--- src/data/items/slotHead.js
+++ src/data/items/slotHead.js
@@ -1,121 +1,245 @@
 export default {
-	headCake: {
-		name: "Cake Hat",
-		sellPrice: 1200,
-		icon: require("@/assets/art/combat/items/head_cakehat.png"),
-		equipmentSlot: "head",
-		overlay: require("@/assets/art/combat/items/head_cakehat_overlay.png"),
-		stats: {
-			maxHealth: 10,
-			evasion: 6,
-			protection: -20,
-			precision: 2,
-			power: 2,
-		},
-		requires: {
-			evasion: 1,
-		}
-	},
-	headH1: {
-		name: "Hard Hat",
-		sellPrice: 1200,
-		icon: require("@/assets/art/combat/items/head_h1.png"),
-		equipmentSlot: "head",
-		overlay: require("@/assets/art/combat/items/head_h1_overlay.png"),
-		stats: {
-			maxHealth: 0,
-			evasion: 1,
-			protection: 0,
-			precision: 1,
-			power: 6,
-		},
-		requires: {
-			evasion: 1,
-		}
-	},
-	headH2: {
-		name: "Welding Helmet",
-		sellPrice: 1200,
-		icon: require("@/assets/art/combat/items/head_h2.png"),
-		equipmentSlot: "head",
-		overlay: require("@/assets/art/combat/items/head_h2_overlay.png"),
-		stats: {
-			maxHealth: 0,
-			evasion: 1,
-			protection: 0,
-			precision: 6,
-			power: 1,
-		},
-		requires: {
-			evasion: 1,
-		}
-	},
 	headRadio1: {
 		name: "Radio Headset",
-		sellPrice: 1200,
-		icon: require("@/assets/art/combat/items/headset_1.png"),
+		sellPrice: 500,
+		icon: require("@/assets/art/combat/items/head/headset_1.png"),
 		equipmentSlot: "head",
-		overlay: require("@/assets/art/combat/items/headset_1_overlay.png"),
+		overlay: require("@/assets/art/combat/items/head/headset_1_overlay.png"),
 		stats: {
 			maxHealth: 0,
-			evasion: 0,
+			evasion: 3,
 			protection: 0,
-			precision: 8,
-			power: 0,
+			precision: 1,
 		},
 		requires: {
-			evasion: 10,
+			evasion: 15,
 		}
 	},	
 	headRadio2: {
 		name: "Ear Protection Headset",
-		sellPrice: 1200,
-		icon: require("@/assets/art/combat/items/headset_2.png"),
+		sellPrice: 500,
+		icon: require("@/assets/art/combat/items/head/headset_2.png"),
 		equipmentSlot: "head",
-		overlay: require("@/assets/art/combat/items/headset_2_overlay.png"),
+		overlay: require("@/assets/art/combat/items/head/headset_2_overlay.png"),
 		stats: {
 			maxHealth: 0,
-			evasion: 0,
-			protection: 4,
+			evasion: 3,
+			protection: 1,
 			precision: 0,
-			power: 0,
 		},
 		requires: {
-			evasion: 10,
+			evasion: 15,
 		}
 	},
 	headRadio3: {
 		name: "Health Sensor Headset",
-		sellPrice: 1200,
-		icon: require("@/assets/art/combat/items/headset_3.png"),
+		sellPrice: 500,
+		icon: require("@/assets/art/combat/items/head/headset_3.png"),
 		equipmentSlot: "head",
-		overlay: require("@/assets/art/combat/items/headset_3_overlay.png"),
+		overlay: require("@/assets/art/combat/items/head/headset_3_overlay.png"),
 		stats: {
-			maxHealth: 16,
-			evasion: 0,
+			maxHealth: 6,
+			evasion: 3,
 			protection: 0,
 			precision: 0,
-			power: 0,
 		},
 		requires: {
-			evasion: 10,
+			evasion: 15,
 		}
 	},
 	headRadio4: {
 		name: "Long Ranged Headset",
 		sellPrice: 1200,
-		icon: require("@/assets/art/combat/items/headset_4.png"),
+		icon: require("@/assets/art/combat/items/head/headset_4.png"),
 		equipmentSlot: "head",
-		overlay: require("@/assets/art/combat/items/headset_4_overlay.png"),
+		overlay: require("@/assets/art/combat/items/head/headset_4_overlay.png"),
+		stats: {
+			maxHealth: 0,
+			evasion: 4,
+			protection: 0,
+			precision: 0,
+		},
+		requires: {
+			evasion: 15,
+		}
+	},
+	headRadio5: {
+		name: "High Volume Headset",
+		sellPrice: 1200,
+		icon: require("@/assets/art/combat/items/head/headset_5.png"),
+		equipmentSlot: "head",
+		overlay: require("@/assets/art/combat/items/head/headset_5_overlay.png"),
+		stats: {
+			maxHealth: 0,
+			evasion: 4,
+			protection: 0,
+			precision: 0,
+			command: 2,
+		},
+		requires: {
+			evasion: 15,
+		}
+	},
+	headH1: {
+		name: "Hard Hat",
+		sellPrice: 1200,
+		icon: require("@/assets/art/combat/items/head/head_h1.png"),
+		equipmentSlot: "head",
+		overlay: require("@/assets/art/combat/items/head/head_h1_overlay.png"),
+		stats: {
+			maxHealth: 0,
+			evasion: 4,
+			protection: 0,
+			precision: 2,
+		},
+		requires: {
+			evasion: 25,
+		}
+	},
+	headH2: {
+		name: "Cakehat",
+		sellPrice: 1200,
+		icon: require("@/assets/art/combat/items/head/head_h2.png"),
+		equipmentSlot: "head",
+		overlay: require("@/assets/art/combat/items/head/head_h2_overlay.png"),
+		stats: {
+			maxHealth: 0,
+			evasion: 4,
+			protection: 2,
+			precision: 0,
+		},
+		requires: {
+			evasion: 25,
+		}
+	},
+	headH3: {
+		name: "Ushanka",
+		sellPrice: 1200,
+		icon: require("@/assets/art/combat/items/head/head_h2.png"),
+		equipmentSlot: "head",
+		overlay: require("@/assets/art/combat/items/head/head_h2_overlay.png"),
+		stats: {
+			maxHealth: 8,
+			evasion: 4,
+			protection: 0,
+			precision: 0,
+		},
+		requires: {
+			evasion: 25,
+		}
+	},
+	headH4: {
+		name: "Security Beret",
+		sellPrice: 1200,
+		icon: require("@/assets/art/combat/items/head/head_h2.png"),
+		equipmentSlot: "head",
+		overlay: require("@/assets/art/combat/items/head/head_h2_overlay.png"),
 		stats: {
 			maxHealth: 0,
 			evasion: 6,
 			protection: 0,
-			precision: 1,
-			power: 1,
+			precision: 0,
 		},
 		requires: {
-			evasion: 10,
+			evasion: 25,
 		}
 	},
+	headH5: {
+		name: "Captain's Hat",
+		sellPrice: 1200,
+		icon: require("@/assets/art/combat/items/head/head_h5.png"),
+		equipmentSlot: "head",
+		overlay: require("@/assets/art/combat/items/head/head_h5_overlay.png"),
+		stats: {
+			maxHealth: 0,
+			evasion: 4,
+			protection: 0,
+			precision: 0,
+			command: 4
+		},
+		requires: {
+			evasion: 25,
+		}
+	},
+	headR1: {
+		name: "Wizard Hat",
+		sellPrice: 5000,
+		icon: require("@/assets/art/combat/items/head/head_r1.png"),
+		equipmentSlot: "head",
+		overlay: require("@/assets/art/combat/items/head/head_r1_overlay.png"),
+		stats: {
+			maxHealth: 0,
+			evasion: 6,
+			protection: 0,
+			precision: 2,
+		},
+		requires: {
+			evasion: 35,
+		}
+	},
+	headR2: {
+		name: "Abductor Interface",
+		sellPrice: 5000,
+		icon: require("@/assets/art/combat/items/head/head_r2.png"),
+		equipmentSlot: "head",
+		overlay: require("@/assets/art/combat/items/head/head_r2_overlay.png"),
+		stats: {
+			maxHealth: 0,
+			evasion: 6,
+			protection: 2,
+			precision: 0,
+		},
+		requires: {
+			evasion: 35,
+		}
+	},
+	headR3: {
+		name: "Plasma Environmental Helm",
+		sellPrice: 5000,
+		icon: require("@/assets/art/combat/items/head/head_r3.png"),
+		equipmentSlot: "head",
+		overlay: require("@/assets/art/combat/items/head/head_r3_overlay.png"),
+		stats: {
+			maxHealth: 8,
+			evasion: 6,
+			protection: 0,
+			precision: 0,
+		},
+		requires: {
+			evasion: 35,
+		}
+	},
+	headR4: {
+		name: "Carp Helmet",
+		sellPrice: 5000,
+		icon: require("@/assets/art/combat/items/head/head_r4.png"),
+		equipmentSlot: "head",
+		overlay: require("@/assets/art/combat/items/head/head_r4_overlay.png"),
+		stats: {
+			maxHealth: 0,
+			evasion: 8,
+			protection: 0,
+			precision: 0,
+		},
+		requires: {
+			evasion: 35,
+		}
+	},
+	headR5: {
+		name: "The Liberator",
+		sellPrice: 5000,
+		icon: require("@/assets/art/combat/items/head/head_r5.png"),
+		equipmentSlot: "head",
+		overlay: require("@/assets/art/combat/items/head/head_r5_overlay.png"),
+		stats: {
+			maxHealth: 0,
+			evasion: 6,
+			protection: 0,
+			precision: 0,
+			command: 4
+		},
+		requires: {
+			evasion: 35,
+		}
+	}
 }
