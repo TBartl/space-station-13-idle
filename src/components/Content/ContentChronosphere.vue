b6f18c01f71d8ab40c6a63569f8d0ee69fe08812
Thomas Bartlett <tbartl@umich.edu>
2020-07-02 00:10:14 -0400

    Add 5 to default chrono speeds

--- src/components/Content/ContentChronosphere.vue
+++ src/components/Content/ContentChronosphere.vue
@@ -89,9 +89,9 @@ export default {
   computed: {
     speeds() {
       if (this.$store.getters["cheats/extraChronoOptions"]) {
-        return [1, 1.5, 2, 2.5, 3, 5, 10, 25, 100, 250, 500, 1000];
+        return [1, 1.5, 2, 3, 5, 10, 25, 100, 250, 500, 1000];
       }
-      return [1, 1.5, 2, 2.5, 3];
+      return [1, 1.5, 2, 3, 5];
     },
     chronoSpeed() {
       return this.$store.getters["chrono/speed"];
