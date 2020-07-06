dfd4cadff0ff5857ba6858ef59b70cffbe9c911b
Thomas Bartlett <tbartl@umich.edu>
2020-07-06 19:23:38 -0400

    Change color setting background

--- src/components/ColorSetter.vue
+++ src/components/ColorSetter.vue
@@ -2,15 +2,29 @@
   <div>
     <div class="d-flex flex-row align-items-center">
       <span class="mr-2">H:</span>
-      <input type="range" v-model="hue" class="form-control-range" min="0" max="360" />
+      <input class="form-control-range hue" type="range" v-model="hue" min="0" max="360" />
     </div>
     <div class="d-flex flex-row align-items-center">
       <span class="mr-2">S:</span>
-      <input type="range" v-model="saturation" class="form-control-range" min="0" max="100" />
+      <input
+        type="range"
+        v-model="saturation"
+        class="form-control-range"
+        :style="satStyle"
+        min="0"
+        max="100"
+      />
     </div>
     <div class="d-flex flex-row align-items-center">
       <span class="mr-2">L:</span>
-      <input type="range" v-model="lightness" class="form-control-range" min="0" max="99" />
+      <input
+        type="range"
+        v-model="lightness"
+        class="form-control-range"
+        :style="lightnessStyle"
+        min="0"
+        max="99"
+      />
     </div>
   </div>
 </template>
@@ -42,7 +56,51 @@ export default {
       set(value) {
         this.$store.commit(`${this.propPath}/setLightness`, value);
       }
+    },
+    satStyle() {
+      return {
+        backgroundImage: `linear-gradient(to right,
+					hsl(${this.hue},0%,${this.lightness}%) 0%,
+					hsl(${this.hue},100%,${this.lightness}%) 100%)`
+      };
+    },
+    lightnessStyle() {
+      return {
+        backgroundImage: `linear-gradient(to right,
+					hsl(${this.hue},${this.saturation}%,0%) 0%,
+					hsl(${this.hue},${this.saturation}%,50%) 50%,
+					hsl(${this.hue},${this.saturation}%,100%) 100%)`
+      };
     }
   }
 };
 </script>
+
+<style scoped>
+.hue {
+  background-image: linear-gradient(
+    to right,
+    red 0%,
+    #ff0 17%,
+    lime 33%,
+    cyan 50%,
+    blue 66%,
+    #f0f 83%,
+    red 100%
+  );
+}
+
+input[type="range"] {
+  -webkit-appearance: none;
+  border-radius: 4px;
+}
+input[type="range"]::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  background-color: white;
+  border: 2px black solid;
+  width: 18px;
+  height: 18px;
+  border-radius: 10px;
+  cursor: pointer;
+}
+</style>
