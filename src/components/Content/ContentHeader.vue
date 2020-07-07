3851a57062df250305a748e84f177667d4dec0d1
Thomas Bartlett <tbartl@umich.edu>
2020-07-06 21:42:17 -0400

    Update settings, move to header

--- src/components/Content/ContentHeader.vue
+++ src/components/Content/ContentHeader.vue
@@ -7,7 +7,7 @@
       <img :src="icon" alt />
       <span>{{text}}</span>
     </div>
-    <div class="player-icon">
+    <div class="player-icon" @click="openCustomization">
       <player-display />
     </div>
   </div>
@@ -17,7 +17,12 @@
 import PlayerDisplay from "@/components/PlayerDisplay";
 export default {
   props: ["icon", "text", "color"],
-  components: { PlayerDisplay }
+  components: { PlayerDisplay },
+  methods: {
+    openCustomization() {
+      this.$store.commit("setVisibleSidebarItem", "customization");
+    }
+  }
 };
 </script>
 
@@ -41,18 +46,18 @@ span {
   color: #373737;
 }
 .player-icon {
-	border: 1px solid rgba(0, 0, 0, 0.376);
+  border: 1px solid rgba(0, 0, 0, 0.376);
   padding: 2px;
   background-color: white;
   width: 38px;
   height: 38px;
   border-radius: 8px;
-	cursor: pointer;
+  cursor: pointer;
   box-shadow: 3px 3px 3px 1px #00000048;
-  transition: transform .15s ease-in-out, background-color .15s ease-in-out;
+  transition: transform 0.15s ease-in-out, background-color 0.15s ease-in-out;
 }
 .player-icon:hover {
-	transform: translateY(-2px);
-	background-color: rgb(227, 235, 240);
+  transform: translateY(-2px);
+  background-color: rgb(227, 235, 240);
 }
 </style>
