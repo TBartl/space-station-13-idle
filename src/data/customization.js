5de8178bf1168cea7e3b2974a7d4a55162060e79
Nabski89 <Nabski@hotmail.com>
2020-07-06 21:35:03 -0400

    Renames and reorders hairs

--- src/data/customization.js
+++ src/data/customization.js
@@ -7,29 +7,59 @@ export const RACES = {
 	lizard: {
 		icon: require("@/assets/art/customization/lizardIcon.png"),
 		skin: require("@/assets/art/customization/LizardMale.png")
+	},
+	moth: {
+		icon: require("@/assets/art/customization/mothIcon.png"),
+		skin: require("@/assets/art/customization/MothMale.png")
+	},
+	ipc: {
+		icon: require("@/assets/art/customization/ipcIcon.png"),
+		skin: require("@/assets/art/customization/IpcMale.png")
 	}
 };
 
 export const HAIR = {
 	bald: require("@/assets/art/customization/hair/bald.png"),
-	thinningrear: require("@/assets/art/customization/hair/thinningrear.png"),
-	_80s: require("@/assets/art/customization/hair/80s.png"),
-	business: require("@/assets/art/customization/hair/business.png"),
-	cia: require("@/assets/art/customization/hair/cia.png"),
-	drillhairextended: require("@/assets/art/customization/hair/drillhairextended.png"),
-	floorlength_bedhead: require("@/assets/art/customization/hair/floorlength_bedhead.png"),
-	kusanagi: require("@/assets/art/customization/hair/kusanagi.png"),
-	modern: require("@/assets/art/customization/hair/modern.png"),
-	ponytail7: require("@/assets/art/customization/hair/ponytail7.png"),
-	poofy: require("@/assets/art/customization/hair/poofy.png"),
-	shavedmohawk: require("@/assets/art/customization/hair/shavedmohawk.png"),
-	sidetail4: require("@/assets/art/customization/hair/sidetail4.png"),
-	spiky: require("@/assets/art/customization/hair/spiky.png"),
-	thinningrear: require("@/assets/art/customization/hair/thinningrear.png"),
-	undercut: require("@/assets/art/customization/hair/undercut.png"),
-	undercutright: require("@/assets/art/customization/hair/undercutright.png"),
-	unshaven_mohawk: require("@/assets/art/customization/hair/unshaven_mohawk.png"),
-	wisp: require("@/assets/art/customization/hair/wisp.png"),
-}
+	thinningrear: require("@/assets/art/customization/hair/m_thinningrear.png"),
+	bigpompadour: require("@/assets/art/customization/hair/m_bigpompadour.png"),
+	cia: require("@/assets/art/customization/hair/m_cia.png"),
+	spiky: require("@/assets/art/customization/hair/m_spiky.png"),
+	undercut: require("@/assets/art/customization/hair/m_undercut.png"),
+	business: require("@/assets/art/customization/hair/m_business.png"),
+	unshaven_mohawk: require("@/assets/art/customization/hair/m_unshaven_mohawk.png"),
+	shavedmohawk: require("@/assets/art/customization/hair/m_shavedmohawk.png"),
+	undercutright: require("@/assets/art/customization/hair/m_undercutright.png"),
 
+	kusanagi: require("@/assets/art/customization/hair/f_kusanagi.png"),
+	poofy: require("@/assets/art/customization/hair/f_poofy.png"),
+	drillhairextended: require("@/assets/art/customization/hair/f_drillhairextended.png"),
+	modern: require("@/assets/art/customization/hair/f_modern.png"),
+	_80s: require("@/assets/art/customization/hair/f_80s.png"),
+	ponytail7: require("@/assets/art/customization/hair/f_ponytail7.png"),
+	sidetail4: require("@/assets/art/customization/hair/f_sidetail4.png"),
+	hime:  require("@/assets/art/customization/hair/f_hime.png"),
+	wisp: require("@/assets/art/customization/hair/f_wisp.png"),
+	floorlength_bedhead: require("@/assets/art/customization/hair/f_floorlength_bedhead.png"),
+};
+
+export const LIZARD = {
+	lizard1: require("@/assets/art/customization/hair/L_H_ram.png"),
+	lizard2: require("@/assets/art/customization/hair/L_H_simple.png"),
+	frill1: require("@/assets/art/customization/hair/L_F_aqua.png"),
+	frill2: require("@/assets/art/customization/hair/L_F_short.png"),
+	frill3: require("@/assets/art/customization/hair/L_F_simple.png"),
+};
+
+export const MOTH = {
+	moth1t: require("@/assets/art/customization/hair/moth_firewatch_top.png"),
+	moth1: require("@/assets/art/customization/hair/moth_firewatch.png"),
+	moth2t: require("@/assets/art/customization/hair/moth_lovers_top.png"),
+	moth2: require("@/assets/art/customization/hair/moth_lovers.png"),
+	moth3t: require("@/assets/art/customization/hair/moth_moonfly_top.png"),
+	moth3: require("@/assets/art/customization/hair/moth_moonfly.png"),
+	moth4t: require("@/assets/art/customization/hair/moth_plain_top.png"),
+	moth4: require("@/assets/art/customization/hair/moth_plain.png"),
+	moth5t: require("@/assets/art/customization/hair/moth_white_top.png"),
+	moth5: require("@/assets/art/customization/hair/moth_white.png"),
+};
 export const BASE_CLOTHING = require("@/assets/art/customization/BaseClothing.png");
