c61a94f89cfa8ea907ea91f137b33365eb506823
Rem <50187357+remona-minett@users.noreply.github.com>
2020-07-11 18:44:47 -0700

    bartending fix menu entry

--- src/data/bartending.js
+++ src/data/bartending.js
@@ -4,7 +4,7 @@ import { MAX_LEVEL } from "@/data/experience";
 const BASIC = {
     synthBeer: {
         item: "beer",
-        // icon: require("@/assets/art/bartending/beer.png"),
+        icon: require("@/assets/art/combat/enemies/missing.png"),
         time: 1,
         xp: .1,
         requiredLevel: 1,
@@ -14,7 +14,7 @@ const BASIC = {
     },
     synthAle: {
         item: "ale",
-        // icon: require("@/assets/art/bartending/ale.png"),
+        icon: require("@/assets/art/combat/enemies/missing.png"),
         time: 1,
         xp: .1,
         requiredLevel: 1,
@@ -24,7 +24,7 @@ const BASIC = {
     },
     synthWhiskey: {
         item: "whiskey",
-        // icon: require("@/assets/art/bartending/whiskey.png"),
+        icon: require("@/assets/art/combat/enemies/missing.png"),
         time: 1,
         xp: .1,
         requiredLevel: 1,
@@ -34,7 +34,7 @@ const BASIC = {
     },
     synthVodka: {
         item: "vodka",
-        // icon: require("@/assets/art/bartending/whiskey.png"),
+        icon: require("@/assets/art/combat/enemies/missing.png"),
         time: 1,
         xp: .1,
         requiredLevel: 1,
@@ -46,7 +46,7 @@ const BASIC = {
 const NA_BASIC = {
     synthLemon: {
         item: "lemonjuice",
-        // icon: require("@/assets/art/bartending/lemonjuice.png"),
+        icon: require("@/assets/art/combat/enemies/missing.png"),
         time: 1,
         xp: .1,
         requiredLevel: 1,
@@ -56,7 +56,7 @@ const NA_BASIC = {
     }, /*
     synthOrange: {
         item: "orangejuice",
-        // icon: require("@/assets/art/bartending/orangejuice.png"),
+        icon: require("@/assets/art/combat/enemies/missing.png"),
         time: 1,
         xp: .1,
         requiredLevel: 1,
@@ -66,7 +66,7 @@ const NA_BASIC = {
     }, */
     synthCream: {
         item: "cream",
-        // icon: require("@/assets/art/bartending/cream.png"),
+        icon: require("@/assets/art/combat/enemies/missing.png"),
         time: 1,
         xp: .1,
         requiredLevel: 1,
@@ -79,7 +79,7 @@ const NA_BASIC = {
 const ALC_T1 = {
     mixBlackRussian: { //t1
         item: "blackrussian",
-        // icon: require("@/assets/art/bartending/blackRussian.png"),
+        icon: require("@/assets/art/combat/enemies/missing.png"),
         time: 2,
         xp: 15,
         requiredLevel: 1,
@@ -91,7 +91,7 @@ const ALC_T1 = {
 const ALC_T2 = {
     mixWhiteRussian: { //t2
         item: "whiterussian",
-        // icon: require("@/assets/art/bartending/whiteRussian.png"),
+        icon: require("@/assets/art/combat/enemies/missing.png"),
         time: 3,
         xp: 25,
         requiredLevel: 3,
@@ -102,15 +102,22 @@ const ALC_T2 = {
     }
 }
 
-const MIN_LEVEL = 1;
+// const MIN_LEVEL = 1;
 Object.values(BASIC).forEach(action => action.type = "ALCOHOLIC BASES");
 Object.values(NA_BASIC).forEach(action => action.type = "NON-ALCOHOLIC BASES");
 Object.values(ALC_T1).forEach(action => action.type = "TIER 1");
 Object.values(ALC_T2).forEach(action => action.type = "TIER 2");
 
+export const ACTIONS = {
+    ...BASIC,
+    ...NA_BASIC,
+    ...ALC_T1,
+    ...ALC_T2
+}
+
 export const JOB = {
     id: "bartending",
     name: "Bartending",
-    // icon: require("@/assets/art/bartending/icon.png"),
+    icon: require("@/assets/art/combat/enemies/missing.png"),
     color: "#3df000" //customize this later //todo
 }
