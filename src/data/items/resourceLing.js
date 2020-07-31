3249ed8f9ca5f01ad09863e8c4ce1f9cb6f06df7
Thomas Bartlett <tbartl@umich.edu>
2020-07-30 22:22:09 -0400

    Remove duplicate "startLing" item

--- src/data/items/resourceLing.js
+++ src/data/items/resourceLing.js
@@ -1,12 +1,2 @@
 export default {
-	startLing: {
-		name: "Harmless Red Slug",
-		sellPrice: 37,
-		icon: require("@/assets/art/ling/headcrab.png")
-	},
-	// DNA: {
-	// 	name: "Polynucleotide Chains",
-	// 	sellPrice: -5,
-	// 	icon: require("@/assets/art/combat/enemies/ai_anim.gif")
-	// }
 }
