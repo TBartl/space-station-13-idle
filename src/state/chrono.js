c160889177b5d252cac3b888dcc29d7825b697a7
Thomas Bartlett <tbartl@umich.edu>
2020-07-31 22:23:15 -0400

    Fix: Remaining time now displays correctly when >24 hours

--- src/state/chrono.js
+++ src/state/chrono.js
@@ -64,7 +64,7 @@ const chrono = {
 			// let hours = `0${}`
 			var seconds = parseInt((duration / 1000) % 60),
 				minutes = parseInt((duration / (1000 * 60)) % 60),
-				hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+				hours = parseInt((duration / (1000 * 60 * 60)));
 
 			hours = hours < 10 ? "0" + hours : hours;
 			minutes = minutes < 10 ? "0" + minutes : minutes;
