8becb509478cb3746b2d3a335c79d525277e79f6
Iamgoofball <iamgoofball@gmail.com>
2023-09-04 05:15:55 -0700

    Genericizes Shitposting

--- src/components/Content/ContentShitposting.vue
+++ src/components/Content/ContentShitposting.vue
@@ -14,7 +14,7 @@
       <job-info
         infoId="shitposting"
         :icon="require('@/assets/art/jobinfo/shitposting.png')"
-        title="Iamgoofball the Shit Poster says..."
+        title="Player420 the Shit Poster says..."
         :options="[
 					{name: 'Back'},
 					{name: 'Shitposting?', icon: require('@/assets/art/shitposting/icon.png'), iconClass:'mx--1'},
@@ -24,31 +24,46 @@
 				]"
       >
         <template slot="Back">
-          <span>WHERES THE SLIP REWRITE WHERE THR FUCK ID IT?</span>
+          <span>This kills tactical chairs. Running behind chairs that are pointed up to obscure your character, making it harder to hit. Yes, i am serious.</span>
         </template>
         <template slot="Shitposting?">
-          <span>🆑 Iamgoofball
-add: How do you do fellow players? I added a new hip and trendy gamemode.
-/🆑</span>
+          <span>This is just like, George Orwell's book, nineteen eighty four.</span>
         </template>
         <template slot="OK?">
-          <span>I have a plan that I'm not revealing publicly until I've started enacting it, because the corrupt pigs in the administration have made it perfectly clear they're going to obstruct like a republican congress when a black guy's in charge. Fuck you, you're not getting details until I'm ramming it in.</span>
+          <span>When they say "people are just going to do [x]" they really mean "I am going to do [x] specifically because this change pisses me off as a form of protest"</span>
         </template>
         <template slot="...">
-          <span>Yeah and I'm pretty sure the community is tired of your shit, dude. You kramer in with these garbage goddamn goal post shifts like some kind of corrupt shitbag. You've actively shifted the goal posts in this thread three goddamn times. I don't know why the fuck you and the administration insist on fucking with me like this. I just wanna contribute to fucking ss13 and be able to speak with the players. Then you let them run roughshod all over me, let them call me homophobic slurs, call me ableist slurs, let them tell me to commit suicide, and then when I finally go "okay, the suicide thing was over the line because the tg admins told me that's over the line" and I try to follow decorum by requesting a moderator step in to de-escalate the situation, you go "WHOA THERE HOW DARE YOU REPORT THE GUY TELLING YOU TO KILL YOURSELF" then make up three different bullshit reasons to ban me, switching to the next when they get disproved.
-
-If this ban is about ban baiting, then the ban is invalid because I'm not fucking ban baiting, and ban baiting isn't a rule on the discord servers anyways so, Jesus christ dude.
-
-If this ban is about drama stirring, then the ban is invalid because I filed proper procedure and directly reported a violation of the Discord TOS, the Server Rules, and the Literal Fucking Thing I Got Banned For 2 Years For to the moderators, and then got banned for it. There was no drama stirring. The only person stirring drama here is you, MSO, by fucking digging in your heels and refusing to accept that maybe the headmins made a bad fucking call and banned me for an illegitimate reason.
-
-If this ban is about my history, then the ban is invalid because as laid out in this thread, I'm the victim of the headmins using me as a fucking whipping post with false accusations of doxxing and false accusations of asking rshoe to ddos the server. And if we're only talking discord history here, the ban is invalid because my serious discord history is "got banned for telling people to kill themselves, successfully appealed, then got banned because I reported someone who told me to kill myself".
-
-Stop acting like a child, MSO. Because this is the sort of underhanded sketchy as fuck bullshit I expect from an underage hippie station player, not the grown ass adult host of /tg/station. I grew up. Why can't you?</span>
+          <span>For your entertainment, here's some of the alphabet of SS13, the variable names of real code:
+		A - Atom, area, admins, augury, advanced disease, arrivals shuttle, asset, award
+		B - Blob, button, blue, area again (for some reason), blindfold, bomb
+		C - Carbon, client, simple_animal in create_random_spawn for some reason, count, corner, circuit, cinematic
+		D - Datum, design, bank account (yes, really), duct, drone
+		E - Radial entry, explosive, eye, ethereal, edible, emitter
+		F - Flat, frame, flasher, fire alarm, fire ball, filter, foam
+		G - Green, ghost, guardian, gloves, goal, high five (again, for seemingly no reason), gang
+		H - Human, holder, hunter, item (as both h and H in drag_drop.dm), holopad
+		I - Item, icon, interview, organ, implant, ID, image, immobilized
+		J - Icon again, job, jaunt, jetpack, cult spell (for seemingly no reason)
+		K - Keycard, knockdown, kinetic blast, knife
+		L - List, living, lighting, laws, limb, lungs, loot, turf
+		M - Mob, month, matrix, mode, machine, message, mobile
+		N - Nuke, new player, new, area for some reason in weather.dm, nozzle, node
+		O - Object, observer, objective, robot (for some reason), closet, outfit, body parts
+		P - Client, paper, node, pack, plumbing, point, player details, projectile
+		Q - Turf, carbon, query, quirk, reagents
+		R - Regex, ref, robot, mech, radio, role
+		S - Silicon, swarmer, shuttle, shelter, start, symptom
+		T - Turf, techweb, quirk, tackle, temperature, cyborg gun
+		U - Under, uplink, user, turf, upgrade
+		V - Vote, vomit, rods, mask, vampire
+		W - Weakref, weather, item, wound, particle, wall
+		X - Xenobio, extract, golem, paper
+		Y - We don't seem to have used Y as a variable name for anything other than y coordinates. Hooray!
+		Z - Plasma tank, drone, victim
+	  </span>
         </template>
         <template slot="I'm leaving now">
-          <span>And additionally, talking to players about PRs that will impact them should really not be cited as "shilling" and a problem. Players need to know what changes are coming down the pipeline.
-
-</span>
+          <span>Before you go, please see this translation guide: https://www.oranges.net.nz/tglist.html</span>
         </template>
       </job-info>
 
