69ca664a5f4f5cc7b526ff54d54e61b78930ccaa
Iamgoofball <iamgoofball@gmail.com>
2023-10-19 07:33:12 -0700

    Update ContentShitposting.vue

--- src/components/Content/ContentShitposting.vue
+++ src/components/Content/ContentShitposting.vue
@@ -19,7 +19,6 @@
 					{name: 'Back'},
 					{name: 'Shitposting?', icon: require('@/assets/art/shitposting/icon.png'), iconClass:'mx--1'},
 					{name: 'OK?'},
-					{name: '...'},
 					{name: 'I\'m leaving now'},
 				]"
       >
@@ -32,38 +31,8 @@
         <template slot="OK?">
           <span>When they say "people are just going to do [x]" they really mean "I am going to do [x] specifically because this change pisses me off as a form of protest"</span>
         </template>
-        <template slot="...">
-          <span>For your entertainment, here's some of the alphabet of SS13, the variable names of real code:
-		A - Atom, area, admins, augury, advanced disease, arrivals shuttle, asset, award
-		B - Blob, button, blue, area again (for some reason), blindfold, bomb
-		C - Carbon, client, simple_animal in create_random_spawn for some reason, count, corner, circuit, cinematic
-		D - Datum, design, bank account (yes, really), duct, drone
-		E - Radial entry, explosive, eye, ethereal, edible, emitter
-		F - Flat, frame, flasher, fire alarm, fire ball, filter, foam
-		G - Green, ghost, guardian, gloves, goal, high five (again, for seemingly no reason), gang
-		H - Human, holder, hunter, item (as both h and H in drag_drop.dm), holopad
-		I - Item, icon, interview, organ, implant, ID, image, immobilized
-		J - Icon again, job, jaunt, jetpack, cult spell (for seemingly no reason)
-		K - Keycard, knockdown, kinetic blast, knife
-		L - List, living, lighting, laws, limb, lungs, loot, turf
-		M - Mob, month, matrix, mode, machine, message, mobile
-		N - Nuke, new player, new, area for some reason in weather.dm, nozzle, node
-		O - Object, observer, objective, robot (for some reason), closet, outfit, body parts
-		P - Client, paper, node, pack, plumbing, point, player details, projectile
-		Q - Turf, carbon, query, quirk, reagents
-		R - Regex, ref, robot, mech, radio, role
-		S - Silicon, swarmer, shuttle, shelter, start, symptom
-		T - Turf, techweb, quirk, tackle, temperature, cyborg gun
-		U - Under, uplink, user, turf, upgrade
-		V - Vote, vomit, rods, mask, vampire
-		W - Weakref, weather, item, wound, particle, wall
-		X - Xenobio, extract, golem, paper
-		Y - We don't seem to have used Y as a variable name for anything other than y coordinates. Hooray!
-		Z - Plasma tank, drone, victim
-	  </span>
-        </template>
         <template slot="I'm leaving now">
-          <span>Before you go, please see this translation guide: https://www.oranges.net.nz/tglist.html</span>
+          <span>It's not your codebase. It's our codebase. You can imply soft power as much as you want, but you don't have it. Division between the server and project is absolute. I'm not interested in reading dezzmont platitudes for the billionth time and won't be checking back in this thread.</span>
         </template>
       </job-info>
 
